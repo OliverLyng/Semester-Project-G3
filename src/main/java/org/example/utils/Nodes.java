171f58ff5388760a2dbbc4dcf05af9ac635b81f0
olyng <ollyn21@student.sdu.dk>
2024-10-28 13:51:14 +0100

    public modifier

--- src/main/java/org/example/utils/Nodes.java
+++ src/main/java/org/example/utils/Nodes.java
@@ -14,33 +14,33 @@ import java.util.concurrent.CompletableFuture;
 public class Nodes {
 
     //Status
-    NodeId stateCurrent = new NodeId(6, "::Program:Cube.Status.StateCurrent");
-    NodeId machSpeedState = new NodeId(6, "::Program:Cube.Status.MachSpeed");
+    public NodeId stateCurrent = new NodeId(6, "::Program:Cube.Status.StateCurrent");
+    public NodeId machSpeedState = new NodeId(6, "::Program:Cube.Status.MachSpeed");
 
 
     //Commands
-    NodeId cntrlCmd = new NodeId(6,"::Program:Cube.Command.CntrlCmd");
-    NodeId cmdChange = new NodeId(6,"::Program:Cube.Command.CmdChangeRequest");
-    NodeId machSpeed = new NodeId(6,"::Program:Cube.Command.MachSpeed");
+    public NodeId cntrlCmd = new NodeId(6,"::Program:Cube.Command.CntrlCmd");
+    public NodeId cmdChange = new NodeId(6,"::Program:Cube.Command.CmdChangeRequest");
+    public NodeId machSpeed = new NodeId(6,"::Program:Cube.Command.MachSpeed");
 
     //Commands/Parameters
-    NodeId parameter0 = new NodeId(6,"::Program:Cube.Command.Parameter[0].Value");
-    NodeId parameter1 = new NodeId(6,"::Program:Cube.Command.Parameter[1].Value");
-    NodeId parameter2 = new NodeId(6,"::Program:Cube.Command.Parameter[2].Value");
+    public NodeId parameter0 = new NodeId(6,"::Program:Cube.Command.Parameter[0].Value");
+    public NodeId parameter1 = new NodeId(6,"::Program:Cube.Command.Parameter[1].Value");
+    public NodeId parameter2 = new NodeId(6,"::Program:Cube.Command.Parameter[2].Value");
 
     //Data
-    NodeId temperature = new NodeId(6,"::Program:Data.Value.Temperature");
+    public NodeId temperature = new NodeId(6,"::Program:Data.Value.Temperature");
 
     //Inventory
-    NodeId barley = new NodeId(6,"::Program:Inventory.Barley");
-    NodeId hops = new NodeId(6,"::Program:Inventory.Hops");
-    NodeId malt = new NodeId(6,"::Program:Inventory.Malt");
-    NodeId wheat = new NodeId(6,"::Program:Inventory.Wheat");
-    NodeId yeast = new NodeId(6,"::Program:Inventory.Yeast");
+    public NodeId barley = new NodeId(6,"::Program:Inventory.Barley");
+    public NodeId hops = new NodeId(6,"::Program:Inventory.Hops");
+    public NodeId malt = new NodeId(6,"::Program:Inventory.Malt");
+    public NodeId wheat = new NodeId(6,"::Program:Inventory.Wheat");
+    public NodeId yeast = new NodeId(6,"::Program:Inventory.Yeast");
 
     //Product
-    NodeId produced = new NodeId(6,"::Program:product.produced");
-    NodeId produce_amount = new NodeId(6,"::Program:product.produce_amount");
+    public NodeId produced = new NodeId(6,"::Program:product.produced");
+    public NodeId produce_amount = new NodeId(6,"::Program:product.produce_amount");
 
 
 
