1083dc97c068758c40b237161fdacd3d3d9ebfb0
OliverLyng <ollyn21@student.sdu.dk>
2024-11-26 11:55:34 +0100

    PUT method to not duplicate data

--- website/app/Http/Controllers/InventoryController.php
+++ website/app/Http/Controllers/InventoryController.php
@@ -14,9 +14,18 @@ class InventoryController extends Controller
         return response()->json($item);
     }
 
+    public function update(Request $request)
+    {
+        $item = Inventory::find($request->id);
+        $item->name=$request->name;
+        $item->quantity=$request->quantity;
+        $result=$item->save();
+        return ["result"=>"Data has been saved"];
+    }
+
     public function show(){
 
         $inventories = Inventory::all();
-        return view('inventory',['items'=>$inventories]);
+        return response()->json($inventories);
     }
 }
